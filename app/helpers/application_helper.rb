627b13391e0ca61134619958dfb2741fba235e4f
Aidan <danburya@grinnell.edu>
2020-02-19 20:29:50 -0600

    Added links to other pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+#Referenced Hartl Textbook
 module ApplicationHelper
+  def full_title(page_title = '')
+  base_title = "Direct Adventures"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "Direct Adventures"
+  end
+  
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get user" do
-    get static_pages_user_url
+    get user_path
     assert_response :success
+    assert_select "title", "User | #{@base_title}"
   end
 
 end
